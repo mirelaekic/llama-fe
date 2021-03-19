3f4a82166acb5ce756b3e7e2cace87d8385ded65
mirelaekic <72197783+mirelaekic@users.noreply.github.com>
2021-03-19 20:59:54 +0100

    adds likes to feedCard

--- src/components/AddComment/AddComment.jsx
+++ src/components/AddComment/AddComment.jsx
@@ -1,21 +1,16 @@
 import { Avatar } from "@material-ui/core";
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, {  useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ChatBubbleOutlineRoundedIcon from '@material-ui/icons/ChatBubbleOutlineRounded';
 import { Col, Row } from "react-bootstrap";
 import "../styles.css";
-import { IconButton } from "@material-ui/core";
 import CommentList from "../commentList/CommentList";
 import { addComment } from "../../store/Actions/comment";
-import { likePost,getLikes } from "../../store/Actions/like";
+
 
 export default function AddComment({ postId }) {
   const dispatch = useDispatch();
   const comments = useSelector((state) => state.comment.allComments);
-  const currentUser = useSelector((state) => state.user.user);
-  const isLiked = useSelector((state) => state.like.isLiked);
+  const currentUser = useSelector((state) => state.user.user);;
   const [comment, setComment] = useState("");
 
   const postComment = (event, comment) => {
@@ -24,27 +19,11 @@ export default function AddComment({ postId }) {
     setComment("");
   }; 
 
-  window.onload = () => (dispatch(getLikes(postId)))
+  //window.onload = () => (dispatch(getLikes(postId)))
 
-  const comm = comments.filter(c => c.postId === postId)
-  const liked = isLiked.includes(currentUser._id)
-  console.log(liked,"if the user is in the array")
 
-  console.log(liked, "if the current user is here ")
   return (
     <>
-      <Row>
-        <Col className="col-post-action">
-          <IconButton onClick={() => dispatch(likePost(postId))}>
-           {liked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
-          </IconButton>
-          <p className="text-muted amount mt-3">{isLiked.length}</p>
-          <IconButton>
-            <ChatBubbleOutlineRoundedIcon />
-          </IconButton>
-              <p className="text-muted amount mt-3">{comm.length}</p> 
-        </Col>
-      </Row> 
       <CommentList postID={postId} />
       <Row>
         <Col className="comment-action">
--- src/components/FeedCard/FeedCard.jsx
+++ src/components/FeedCard/FeedCard.jsx
@@ -6,21 +6,30 @@ import { useDispatch, useSelector } from "react-redux";
 import { getUsers } from "../../store/Actions/user";
 import Moment from "react-moment"
 import AddComment from "../AddComment/AddComment"
-
+import { likePost,getLikes } from "../../store/Actions/like";
+import { IconButton } from "@material-ui/core";
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ChatBubbleOutlineRoundedIcon from '@material-ui/icons/ChatBubbleOutlineRounded';
 export default function FeedCard() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getUsers());
   }, []);
-
+  const comments = useSelector((state) => state.comment.allComments);
+  const isLiked = useSelector((state) => state.like.isLiked)
   const posts = useSelector((state) => state.post.allPosts);
   const allUsers = useSelector((state) => state.user.allUsers);
-
+  const comm = comments.filter(c => c.postId === postID)
+  const liked = isLiked.includes(currentUser._id)
+  console.log(isLiked,"the post that is liked")
+  console.log(liked,"if the user is in the array")
   return (
     <>
       {posts ?
         posts.map((p, i) => (
+          <>
           <Card key={i} className="mt-2 mb-2" style={{ width: "auto" }}>
             <Card.Header className="user-info ml-2">
               {allUsers && allUsers.map((user, i) =>
@@ -50,6 +59,20 @@ export default function FeedCard() {
              <AddComment postId={p._id} />
             </Card.Body>
           </Card>
+          <Row>
+        <Col className="col-post-action">
+          <IconButton onClick={() => dispatch(likePost(postId))}>
+           {liked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+          </IconButton>
+          <h6 onClick={()=> dispatch(getLikes(postId))}>h</h6>
+          <p className="text-muted amount mt-3">{isLiked.length}</p>
+          <IconButton>
+            <ChatBubbleOutlineRoundedIcon />
+          </IconButton>
+              <p className="text-muted amount mt-3">{comm.length}</p> 
+        </Col>
+      </Row> 
+          </>
         )) : <CircularProgress/>}
     </>
   );
